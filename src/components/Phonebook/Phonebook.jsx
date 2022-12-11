835e3aaca11908700288c42dd0cc98d44451950b
YriyProtcyshin <105303233+YriyProtcyshin@users.noreply.github.com>
2022-12-11 11:52:26 +0200

    commit

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,12 +1,19 @@
 import { Component } from 'react';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import css from './Phonebook.module.css';
 import { nanoid } from 'nanoid';
+import * as yup from "yup"
+
 
 const initialValues = {
   name: '',
   number: '',
 };
+
+let schema = yup.object().shape({
+  name: yup.string().required(),
+  number: yup.number().integer().positive().required()
+})
 
 export class Phonebook extends Component {
   onAddButtonClick = (values, active) => {
@@ -15,22 +22,24 @@ export class Phonebook extends Component {
   };
   render() {
     return (
-      <Formik initialValues={initialValues} onSubmit={this.onAddButtonClick}>
+      <Formik initialValues={initialValues} onSubmit={this.onAddButtonClick} validationSchema={schema}>
         <Form className={css.form}>
           <p className={css.inputLabel}>Name</p>
           <Field
             className={css.field}
             type="text"
             name="name"
-            placeholder="Name"
+            placeholder="Rosie Simpson"
           />
+          <ErrorMessage name="name" component= "div" className= {css.errorMessage}/>
           <p className={css.inputLabel}>Number</p>
           <Field
             className={css.field}
             type="tel"
             name="number"
-            placeholder="000-00-00"
+            placeholder="2343635"
           />
+          <ErrorMessage name="number" component= "div" className= {css.errorMessage}/>
           <button type="submit">Add contact</button>
         </Form>
       </Formik>
