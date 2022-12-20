9d5027ff3544d3a7c734ff07a942812c0b51a628
YriyProtcyshin <105303233+YriyProtcyshin@users.noreply.github.com>
2022-12-20 13:28:22 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
-import { Component } from 'react';
+
 import { Phonebook } from './Phonebook/Phonebook';
 import { Contacts } from './Contacts/Contacts';
 import { Filter } from './Filter/Filter';
 import css from './App.module.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 const LS_KEY = "local_stoge_contacts"
 
@@ -16,22 +17,46 @@ const initialContacts =  [
 ]
     
 export function App(){
-  const [contacts, setContacts] = useState(initialContacts)
-  const [name, setName] = useState('')
+  const [contacts, setContacts] = useState(() => {    
+    if (localStorage.getItem(LS_KEY)) {
+      return JSON.parse(localStorage.getItem(LS_KEY))
+    } 
+    return initialContacts
+  })
+  
   const [filter, setFilter] = useState('')
 
-  const addContact = (id, name, number) => {
+  useEffect(() => { 
+    localStorage.setItem(LS_KEY, JSON.stringify(contacts))
+  }, [contacts])
+
+ 
+
+  function addContact (id, name, number) {
     if (contacts.find(contact => contact.name === name)) {
       Notify.failure(`${name} is alredy in contacts`);
       return
     }
+
+    number = number.split('')
+    number.splice(3,0,"-")
+    number.splice(6,0,"-")
+    number = number.join('')   
+
+
     setContacts([...contacts, {id, name, number}])
   }
 
-  const onDelete = () => {
-    return ""
+  function onDelete (idContact) {
+    setContacts(contacts.filter(contact => contact.id !== idContact))
   }
-  const filteredContacts = contacts.filter(contact => contact.name.includes(filter.toLowerCase()))
+
+  function onFilter(event) {
+    setFilter(event.currentTarget.value)
+  }
+
+  const filteredContacts = contacts.filter(
+    contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
 
 
 
@@ -39,8 +64,8 @@ export function App(){
     <div className={css.phonebook}>
       <h1>Phonebook</h1>
       <Phonebook addContact={addContact} />
-      <Contacts contacts={filteredContacts} handleDelete={this.onDelete}>
-        {/* <Filter onFilter={this.onFilter} /> */}
+      <Contacts contacts={filteredContacts} handleDelete={onDelete}>
+        <Filter onFilter={onFilter} />
       </Contacts>
     </div>
   );
